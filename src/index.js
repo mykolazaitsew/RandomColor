bc4a59af3bf0ba843188a2b73c00e799c456b786
Zaitsev Mykola <zaitsev.nikolay11@gmail.com>
2024-11-24 01:21:25 +0200

    full

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const randomColor = () => {
+        const randomNumber = () => Math.round(Math.random() * 255);
+
+       const color = [];
+
+        for (let i = 0; i < 3; i++) {
+            const num = randomNumber();
+            color.push(num)
+        }
+        console.log(color);
+        
+        return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;        
+}
+
+const updateColor = (color) => {
+    document.querySelector('h2').textContent = `Color = ${color}`;
+}
+
+
+document.getElementById('colorBtn').addEventListener('click', () =>{
+    const newColor = randomColor(); 
+    document.body.style.backgroundColor = newColor;
+    updateColor(newColor);
+});
