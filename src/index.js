453b95647940034cca34f4b9cd74592cf4d953dc
Zaitsev Mykola <zaitsev.nikolay11@gmail.com>
2024-11-25 18:48:27 +0200

    copy the rgb color you want

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-const randomColor = () => {
+class RandomColor {
+    constructor(buttonId, displayId) {
+        this.button = document.getElementById(buttonId);
+        this.display = document.getElementById(displayId);
+        this.addColorChangeListener();
+    }
+
+    addColorChangeListener() {
+        this.button.addEventListener("click", () => this.changeColor());
+    }
+
+    changeColor() {
+        const newColor = this.randomColor();
+        document.body.style.backgroundColor = newColor;
+        this.updateColorDisplay(newColor);
+    }
+
+    randomColor() {
         const randomNumber = () => Math.round(Math.random() * 255);
 
        const color = [];
@@ -9,16 +26,34 @@ const randomColor = () => {
         }
         console.log(color);
         
-        return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;        
+        return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    }
+
+    updateColorDisplay(color) {
+        document.querySelector('h2').textContent = `Color = ${color}`;  
+    }
 }
 
-const updateColor = (color) => {
-    document.querySelector('h2').textContent = `Color = ${color}`;
-} 
+class CopyText {
+    constructor(elementId) {
+        this.element = document.getElementById(elementId);
+        this.addCopyEventListener();
+    }
+
+    addCopyEventListener() {
+        this.element.addEventListener("click", () => this.copyText());
+    }
+
+    copyText() {
+        const tempInput = document.createElement("input");
+        tempInput.value = this.element.textContent;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+    }
+}
 
 
-document.getElementById('colorBtn').addEventListener('click', () =>{
-    const newColor = randomColor(); 
-    document.body.style.backgroundColor = newColor;
-    updateColor(newColor);
-});
+new RandomColor('colorBtn', 'colorDisplay');
+new CopyText('textToCopy');
